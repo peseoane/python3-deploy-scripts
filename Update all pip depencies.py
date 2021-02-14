ff12a1494eef49b87546ed614bbeaca1491fc1a0
Pedro Vicente Seoane Prado <info@pedroseoaneprado.es>
2021-02-14 10:36:46 +0100

    Add 2 scripts for pip and Jupyterlab
    This scripts DO NOT WORK on pip < 10x.

---
+++ Update all pip depencies.py
@@ -0,0 +1,10 @@
+# GNU LESSER GENERAL PUBLIC LICENSE
+# AUTHOR: PEDRO VICENTE SEOANE PRADO (SPAIN)
+# https://github.com/peseoane/python3-deploy-scripts/
+
+import pkg_resources
+from subprocess import call
+
+packages = [dist.project_name for dist in pkg_resources.working_set]
+call("pip install -U pip wheel --upgrade ", shell=True)
+call("pip install -U --upgrade " + " ".join(packages), shell=True)
